0e46103413a38b3517a3e3e1b0f4d3eefeecbce4
edgar97-hub <ed.quispe.mamani@isur.edu.pe>
2025-05-15 16:44:21 -0500

    .

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,9 +5,14 @@ import * as morgan from 'morgan';
 import { CORS } from './constants';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { Request, Response } from 'express';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(morgan('dev'));
   app.useGlobalPipes(
     new ValidationPipe({
@@ -32,6 +37,18 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.use((req: Request, res: Response) => {
+    res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  });
+
+  // app.useStaticAssets(join(__dirname, '..', 'public')); // donde está Angular
+  // app.setBaseViewsDir(join(__dirname, '..', 'public'));
+
+  // // catch-all route para SPA
+  // app.use('*', (req: Request, res: Response) => {
+  //   res.sendFile(join(__dirname, '..', 'public', 'index.html'));
+  // });
   await app.listen(configservice.get('PORT') ?? 3000);
   console.log(`Application running on: ${await app.getUrl()}`);
 }
