bed865e3ed97fca564294086e0d591cf3138e1e6
edgar97-hub <ed.quispe.mamani@isur.edu.pe>
2025-05-21 16:20:08 -0500

    .

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://161.132.41.184:8000/api',
-  // apiUrl: 'http://localhost:8000/api',
+  // apiUrl: 'http://161.132.41.184:8000/api',
+  apiUrl: 'http://localhost:8000/api',
 };
